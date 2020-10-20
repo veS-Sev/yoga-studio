8b89f70fba1a5408b82c7de2195f2bb745e9ed4e
ves-sev <newsecretword@yandex.ru>
2020-10-20 20:43:45 +0300

    header changed, aboutUs added but not finish

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,7 @@ $(function () {
   var mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 4,
     spaceBetween: 30,
-    // loop: true,
-    freeMode: true,
+     freeMode: true,
 
     // Navigation arrows
     navigation: {
@@ -14,6 +13,15 @@ $(function () {
 
   });
 
-
+  var swiper = new Swiper('.aboutUS__feedback-swiper', {
+    slidesPerView: 1,
+    effect: 'fade',
+    freeMode: true,
+    // spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 
 });
